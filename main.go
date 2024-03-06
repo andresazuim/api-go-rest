bc2f7c5f6b3bcc11d7532dcaae6b48ba99b6dc9a
andresazuim <andresazuim37@gmail.com>
2024-03-06 19:08:49 -0300

    implementação do docker

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 func main() {
 
 	models.Personalidades = []models.Personalidade{
-		{Nome: "nome1", Historia: "historia 1"},
-		{Nome: "nome2", Historia: "historia 2"},
+		{Id: 1, Nome: "nome1", Historia: "historia 1"},
+		{Id: 2, Nome: "nome2", Historia: "historia 2"},
 	}
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
